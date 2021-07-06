361aa27d1f32c22a46dc4f15ca41904634b217b4
Dohd <alloicebrian@gmail.com>
2021-07-06 23:45:13 +0300

    Update socketio origin

--- utils/socketio.js
+++ utils/socketio.js
@@ -25,18 +25,13 @@ const socketEvents = [
     'users',
 ];
 
-const getOrigin = () => (
-    process.env.NODE_ENV === 'production' ? 
-    process.env.SOCKET_URL : process.env.CLIENT_URL
-);
-
 // Use Redis-db 
 const accounts = [];
 module.exports = function(server) {
     // Initialize socket
     const io = new Server(server, {
         cors: {
-            origin: getOrigin(),
+            origin: process.env.CLIENT_URL,
             methods: ['GET','POST'],
             credentials: true
         }

b6c3b1e816802062f8e4c096c8c4a0a7e23e4405
Dohd <alloicebrian@gmail.com>
2021-08-27 23:36:06 +0300

    Create raw query sql string and function

--- controllers/ProgrammeGraph.js
+++ controllers/ProgrammeGraph.js
@@ -1,7 +1,7 @@
-const { Op } = require('../utils/database');
 const moment = require('moment');
-const { Participant } = require('../models/Participant');
-const { KeyProgramme } = require('../models/Essential');
+const { QueryTypes } = require('sequelize');
+
+const { db } = require('../utils/database');
 
 module.exports = {
     findAll: async (req, res, next) => {
@@ -9,47 +9,33 @@ module.exports = {
             const accountId = req.payload.aud;
 
             const yr = new Date().getFullYear();
-            const jan = new Date(yr, 0, 1);
-            const dec = new Date(yr, 11, 31);
-            const yr_range = [jan, dec].map(v => {
-                return  moment(v).format('YYYY-MM-DD');
+            const dates = [
+                new Date(yr, 0, 1), 
+                new Date(yr, 11, 31)
+            ].map(v => moment(v).format('YYYY-MM-DD'));
+            const fromDate = req.query.from || dates[0];
+            const toDate = req.query.to || dates[1];
+            
+            // participants count per programme
+            const queryStr = `
+                SELECT
+                    prog.id,
+                    prog.programme,
+                    SUM(CASE WHEN p.gender = 'M' THEN 1 ELSE 0 END) male,
+                    SUM(CASE WHEN p.gender = 'F' THEN 1 ELSE 0 END) female,
+                    COUNT(p."keyProgrammeId") total
+                FROM key_programmes prog
+                INNER JOIN participants p
+                    ON prog.id = p."keyProgrammeId"
+                WHERE p."activityDate" BETWEEN :fromDate AND :toDate
+                AND prog."accountId" = :accountId
+                GROUP BY prog.id
+            `;
+
+            const dataset = await db.query(queryStr, {
+                replacements: {accountId, fromDate, toDate},
+                type: QueryTypes.SELECT
             });
-
-            const fromDate = req.query.from || yr_range[0];
-            const toDate = req.query.to || yr_range[1];
-
-            const participants = await Participant.findAll({
-                where: { 
-                    accountId,
-                    activityDate: {
-                        [Op.between]: [fromDate, toDate]
-                    }
-                },
-                attributes: ['id','gender','keyProgrammeId']
-            });
-
-            const programmes = await KeyProgramme.findAll({
-                where: { accountId },
-                attributes: ['id']
-            });
-
-            const dataset = { male: [], female: [] };
-
-            for (const programme of programmes) {
-                let maleCount = 0;
-                let femaleCount = 0;
-
-                for (const p of participants) {
-                    const programme_match = p.keyProgrammeId === programme.id;                    
-                    if (programme_match) {
-                        if (p.gender === 'M') maleCount++;
-                        if (p.gender === 'F') femaleCount++;
-                    }                    
-                }
-                dataset.male.push(maleCount);
-                dataset.female.push(femaleCount);
-            }
-
             res.send(dataset);
         } catch (error) {
             next(error);
